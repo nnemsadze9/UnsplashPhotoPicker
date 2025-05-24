2eeef52eae553b8a8580bd41306288fd7fd454de
nika <nika.nemsadze@makingscience.com>
2025-05-24 17:54:18 +0400

    Initial Commit

---
+++ Unsplash Photo Picker/Unsplash_Photo_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unsplash_Photo_PickerApp.swift
+//  Unsplash Photo Picker
+//
+//  Created by User on 24.05.25.
+//
+
+import SwiftUI
+
+@main
+struct Unsplash_Photo_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Unsplash Photo PickerTests/Unsplash_Photo_PickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Unsplash_Photo_PickerTests.swift
+//  Unsplash Photo PickerTests
+//
+//  Created by User on 24.05.25.
+//
+
+import Testing
+@testable import Unsplash_Photo_Picker
+
+struct Unsplash_Photo_PickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
